2acb27e86950e86669b76991b58d4630a70a440c
Dark2C <41321160+Dark2C@users.noreply.github.com>
2025-02-14 16:47:02 +0100

    Script batch per generazione chiavi EC con Docker (correzione typo)

--- Varie/generate_keys/generate_keys.bat
+++ Varie/generate_keys/generate_keys.bat
@@ -7,7 +7,7 @@ echo RUN openssl ec -in private.pem -pubout -out public.pem
 
 echo FROM scratch
 echo COPY --from=builder private.pem /private.pem
-echo COPY --from=builder public.pem /public..pem
+echo COPY --from=builder public.pem /public.pem
 ) > Dockerfile
 docker build --output type=tar,dest=keys.tar -f Dockerfile .
 del Dockerfile
